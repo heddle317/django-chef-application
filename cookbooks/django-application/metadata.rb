7663bdf9793d303bc2d3c93337a37a163556f9b5
Kate Heddleston <kate.heddleston@gmail.com>
2013-02-21 14:25:51 -0800

    Add django application installations. Really basic example of the types of installations you can do in a cookbook.

---
+++ cookbooks/django-application/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Kate Heddleston"
+maintainer_email "kate.heddleston@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures webserver-application"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
---
+++ cookbooks/django-application/recipes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: webserver-application
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# create a 2.6 virtualenv owned by ubuntu user
+# python_virtualenv "/home/ubuntu/flask_ve" do
+#     owner "root"
+#     group "sysadmins"
+#     action :create
+# end
+
+python_pip "Django" do
+    action :install
+end
+
+python_pip "simplejson" do
+    action :install
+end
+
+python_pip "pytz" do
+    action :install
+end
+
+python_pip "distribute" do
+    action :install
+end
+
+python_pip "wsgiref" do
+    action :install
+end
